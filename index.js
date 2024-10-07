1ce4317c24f46e0b59ffd0c1772a62b9b1a6893b
tenok94 <tenok94@gmail.com>
2024-10-07 15:36:09 -0300

    mejorando la api con busqueda de libros

--- index.js
+++ index.js
@@ -6,6 +6,33 @@ app.get('/hello', (req, res) => {
     res.send('Hola mundo');
 });
 
+app.get('/', (req, res) => {
+    res.send('Bienvenidos a mi Biblioteca');
+});
+
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
 });
+
+//datos
+
+const libros = [
+    { id: 1, titulo: 'El Quijote', autor: 'Miguel de Cervantes' },
+    { id: 2, titulo: 'La Divina Comedia', autor: 'Dante Alighieri' },
+    { id: 3, titulo: '1984', autor: 'George Orwell' }
+];
+
+// get all
+app.get("/libros", (req, res) => {
+    res.json(libros);
+})
+
+//get one
+app.get("/libros/:id", (req, res) => {
+    const libro = libros.find(li => li.id === parseInt(req.params.id)); 
+    if (libro){
+        res.json(libro);
+    }  else {
+        res.status(404).send("libro no encontrado");
+    }
+})
