73bf0ad4f3ed219e48391ef675645098b245cbdf
tenok94 <tenok94@gmail.com>
2024-10-09 23:27:20 -0300

    agregamos y eliminamos libros

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ const libros = [
     { id: 3, titulo: '1984', autor: 'George Orwell' }
 ];
 
-// get all
+// llama a todos los libros que tenemos en el array
 app.get("/libros", (req, res) => {
     res.json(libros);
 })
 
-//get one
+//llama un libro especifico en el array
 app.get("/libros/:id", (req, res) => {
     const libro = libros.find(li => li.id === parseInt(req.params.id)); 
     if (libro){
@@ -35,4 +35,30 @@ app.get("/libros/:id", (req, res) => {
     }  else {
         res.status(404).send("libro no encontrado");
     }
+})
+
+// crear un libro
+app.use(express.json()); //recibir en el body un json
+
+app.post("/libros", (req, res) => {
+
+    const nuevoLibro = {id: libros.length + 1, ...req.body}; //id sera 1 + que el ultimo libro
+    //libbros.length es el tamaño del arreglo y a eso se le suma 1
+    libros.push(nuevoLibro); //añade ese nuevo libro al array;
+    res.status(201).json(nuevoLibro);
+
+})
+
+//elimina un libro
+app.delete("/libros/:id", (req, res) => {
+    const idLibro = parseInt(req.params.id); // convierte el parametro en id del libro
+    const indiceLibro = libros.findIndex(li => li.id === idLibro); // busca el indice del libro en el array
+   
+    if (indiceLibro !== -1){
+        const libroEliminado = libros.splice(indiceLibro, 1); //elimina el libro del array
+        res.json({ mensaje: "libro eliminado", libro: libroEliminado[0]});
+    }else{
+        res.status(404).json({ mensaje: "libro no encontrado"}); // si no se encuentra el libro, devuelve error
+    }
+
 })
